bab4928a6e119bdf1eb61154ee76301062de52c3
RobsonArcoleze <robson.arcoleze29@gmail.com>
2025-03-14 20:37:06 -0300

    Create Project

---
+++ src/main/java/br/com/tokiomarine/safebankingapi/SafeBankingApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tokiomarine.safebankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeBankingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeBankingApiApplication.class, args);
+    }
+
+}
